f516f057012570f0ec9bf15ef9ac348b0b3583b1
myambitions <daurenbekzhanov@gmail.com>
2019-09-02 00:35:02 +0600

    Updates

---
+++ component---src-pages-page-2-js-96d8fac2f4639b9b5727.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{190:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(200),c=a(203),r=a(202);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-96d8fac2f4639b9b5727.js.map
---
+++ styles-9852ea510d577b161303.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{244:function(n,w,o){}}]);
+//# sourceMappingURL=styles-9852ea510d577b161303.js.map
